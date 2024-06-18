9b49b7995d09469928410b1da6009ffcdef3d8c3
Mogomotsi Tshwagong <mmoroane@hotmail.com>
2024-06-18 09:44:05 +0200

    Added active player functionality

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,32 @@
+import { useState } from 'react';
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ];
 
-const GameBoard = () => {
+const GameBoard = ({ onSelectSquare, activePlayerSymbol }) => {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    const handleSelectSquare = (rowIndex, colIndex) => {
+        setGameBoard((previousGameBoard) => {
+            const updatedBoard = [...previousGameBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+            return updatedBoard;
+        });
+
+        onSelectSquare();
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
+            {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button>{playerSymbol}</button>
+                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                             </li>
                         ))}
                     </ol>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const Player = ({ initialName, symbol }) => {
+const Player = ({ initialName, symbol, isActive }) => {
     // console.log("Player Component rendered (" + symbol + ")");
 
     const [playerName, setPlayerName] = useState(initialName);
@@ -25,7 +25,7 @@ const Player = ({ initialName, symbol }) => {
     }
 
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className="player">
                 {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
