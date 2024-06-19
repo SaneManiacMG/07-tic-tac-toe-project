ac6374037e1bec83f683822b3086dc331f3bf23b
Mogomotsi Tshwagong <mmoroane@hotmail.com>
2024-06-19 08:21:30 +0200

    Added conditional clicking to board

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -21,7 +21,11 @@ const GameBoard = ({ onSelectSquare, turns }) => {
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                                <button
+                                    onClick={() => onSelectSquare(rowIndex, colIndex)}
+                                    disabled={playerSymbol !== null}>
+                                    {playerSymbol}
+                                </button>
                             </li>
                         ))}
                     </ol>
